5e0bf0ee8a47164834335e9c198add3c6fcf36f9
Jhosep Sanchez Remigio <jhosep.remigio2005.com>
2024-12-26 18:03:20 -0500

    Cambios

---
+++ js/Card.js
@@ -0,0 +1,37 @@
+const iconCart =`<img src="assets/images/icon-add-to-cart.svg" class="cart-icon">`
+const iconDecrement =`<img class="icon-decrementar" src="assets/images/icon-decrement-quantity.svg">`
+const iconIncrement =`<img class="icon-incrmentar" src="assets/images/icon-increment-quantity.svg">`
+
+function Card() {
+  fetch("data.json")
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      let card = "";
+      data.forEach((info) => {
+        card += `
+            <div class="product-card">
+            <div class="image-container">
+            <img src="${info.image.desktop}" alt="${info.category}" class="product-image">
+            <div class="quantity-control">
+            <button class="quantity-btn">${iconDecrement}</button>
+            <span class="quantity">4</span>
+            <button class="quantity-btn">${iconIncrement}</button>
+            </div>
+            <button class="add-to-cart">${iconCart}
+            Add to Cart
+            </button>
+            </div>
+            <div class="category">${info.category}</div>
+            <div class="product-name">${info.name}</div>
+            <div class="price">$${info.price}</div>
+        </div>
+            `;
+
+        document.getElementById("lista-productos").innerHTML = card;
+      });
+    });
+}
+
+export { Card };
